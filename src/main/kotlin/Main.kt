69eeb59e58674e4be50fbfdbb77633a5bae35c5e
Divyanshu Kumar <divyanshuk10@gmail.com>
2021-09-24 00:51:42 +0530

    Numbers - getodd solution-2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,3 @@
-import number.ListOfOdd
 import strings.Solution1
 import strings.Solution2
 import strings.Solution3
@@ -7,6 +6,6 @@ fun main() {
 //    println(Solution1.capitalizeSentence("hello world"))
 //    println(Solution2.capitalizeSentence("hello world how are you"))
 //    println(Solution3.capitalizeSentence("hello world how are you"))
-    println(ListOfOdd.getOdd(listOf(1,3,4,5,6,7,0)))
+    println(number.Solution2.getOdd(listOf(1, 3, 4, 5, 6, 7, 0)))
 
 }
--- src/main/kotlin/number/GetOdd.kt
+++ src/main/kotlin/number/GetOdd.kt
@@ -14,16 +14,31 @@ import org.junit.jupiter.api.Test
 *
 * */
 
-object ListOfOdd {
+object Solution1 {
     fun getOdd(list: List<Int>): List<Int> {
         return list.filter { i -> i % 2 != 0 }
     }
 }
 
+// recursive approach
+object Solution2 {
+    fun getOdd(list: List<Int>): List<Int> {
+        if (list.isEmpty()) {
+            return list
+        }
+        return if (list.first() % 2 == 1) {
+            mutableListOf(list.first()) + getOdd(list.drop(1))
+        } else {
+            getOdd(list.drop(1))
+        }
+    }
+}
+
+
 private class Test {
 
     @Test
     fun `"filter all odd items in a list"`() {
-        Assertions.assertEquals(ListOfOdd.getOdd(listOf(1, 2, 3, 4, 5, 666, 77)), listOf(1, 3, 5, 77))
+        Assertions.assertEquals(Solution1.getOdd(listOf(1, 2, 3, 4, 5, 666, 77)), listOf(1, 3, 5, 77))
     }
 }
